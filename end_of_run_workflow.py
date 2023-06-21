9cead5f820e25db2c9804b6365ced80d4bb4f133
Abigail Giles <agiles@bnl.gov>
2023-06-21 13:23:41 -0400

    Add data syncing to end of run workflow

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -2,6 +2,7 @@ from prefect import flow, get_run_logger, task
 
 from analysis import analysis_flow
 from data_validation import general_data_validation
+from export import export
 
 
 @task
@@ -15,5 +16,6 @@ def end_of_run_workflow(stop_doc):
     uid = stop_doc["run_start"]
 
     general_data_validation(beamline_acronym="cms", uid=uid)
+    export(ref=uid, subdirs=True)
 
     # analysis_flow(raw_ref=uid)
