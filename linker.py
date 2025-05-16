9afe025c321b3db1bc8bc2ae4501daa0c22da5ea
Eugene M <ymatviych@bnl.gov>
2025-05-16 14:11:12 -0400

    WIP: moved handle_file from CMS profile-collection

--- linker.py
+++ linker.py
@@ -1,138 +1,38 @@
-from prefect import flow, task, get_run_logger
+from prefect import task, get_run_logger
 from pathlib import Path
 from tiled.client import from_profile
-
-import event_model
-import tqdm
-import shutil
+import os
+import glob
 
 tiled_client = from_profile("nsls2")["smi"]
 tiled_client_raw = tiled_client["raw"]
 
-
-def handle_file(self, detector, extra=None, verbosity=3, subdirs=True, linksave=True, **md):
-    subdir = ""
-    if subdirs:
-        if detector.name == "pilatus300" or detector.name == "pilatus8002":
-            subdir = "/maxs/raw/"
-            detname = "maxs"
-        elif detector.name == "pilatus2M":
-            subdir = "/saxs/raw/"
-            detname = "saxs"
-        elif detector.name == "pilatus800":
-            subdir = "/waxs/raw/"
-            detname = "waxs"
-        else:
-            if verbosity >= 1:
-                print("WARNING: Can't do file handling for detector '{}'.".format(detector.name))
-                return
-
-    filename = detector.tiff.full_file_name.get()  # RL, 20210831
-    if not os.path.isfile(filename):
-        print("File does not exist")
-        return 
-    # Alternate method to get the last filename
-    # filename = '{:s}/{:s}.tiff'.format( detector.tiff.file_path.get(), detector.tiff.file_name.get()  )
-
-    if verbosity >= 3:
-        print("  Data saved to: {}".format(filename))
-
-    # if md['measure_type'] is not 'snap':
-    if True:
-        # self.set_attribute('exposure_time', caget('XF:11BMB-ES{Det:SAXS}:cam1:AcquireTime'))
-        self.set_attribute("exposure_time", detector.cam.acquire_time.get())  # RL, 20210831
-
-        # Create symlink
-        # link_name = '{}/{}{}'.format(RE.md['experiment_alias_directory'], subdir, md['filename'])
-        # savename = md['filename'][:-5]
-
-        # savename = self.get_savename(savename_extra=extra)
-        savename = md["filename"]
-        # link_name = '{}/{}{}_{:04d}_maxs.tiff'.format(RE.md['experiment_alias_directory'], subdir, savename, RE.md['scan_id']-1)
-        link_name = "{}/{}{}_{}.tiff".format(RE.md["experiment_alias_directory"], subdir, savename, detname)
-
-        if os.path.isfile(link_name):
-            i = 1
-            while os.path.isfile("{}.{:d}".format(link_name, i)):
-                i += 1
-            os.rename(link_name, "{}.{:d}".format(link_name, i))
-        os.symlink(filename, link_name)
-
-        if verbosity >= 3:
-            print("  Data linked as: {}".format(link_name))
-            if not os.path.isfile(os.readlink(link_name)): #added by RL, 20231109
-                raise ValueError('NO IMAGE OUTPUT.')
+logger = get_run_logger()
 
 
-def do_symlinking(
-    links: list[tuple[str, Path, Path]],
-    overwrite_dest=False,
-) -> tuple[list[tuple[str, Path, Path]], list[tuple[str, Path, Path]]]:
-    """Create the symlinks, making target directories as needed.
-
-    Paramaters
-    ----------
-    links : list of (uid, src, dest) tuples
-        The uid, source file and destination files
-
-    overwrite_dest : bool, optional
-        If an existing destitation should be unlinked and replaced.
-
-    Returns
-    -------
-    linked, failed : list of (uid, src, dest) tuples
-        The linked (or failed) values.
-    """
-
-    failed, linked = [], []
-
-    for uid, src, dest, analysis in tqdm.tqdm(links, leave=False):
-        if not src.exists():
-            failed.append((uid, src, dest, analysis))
-            continue
-
-        try:
-            dest.parent.mkdir(exist_ok=True, parents=True)
-
-            if not analysis.exists():
-                # copy the default analysis notebooks to the analysis directory
-                default_analysis_path_s = Path('/nsls2/data/smi/shared/default_nb/saxs.ipynb')
-                default_analysis_path_w = Path('/nsls2/data/smi/shared/default_nb/waxs.ipynb')
-                
-                analysis.mkdir(exist_ok=True, parents=True)
-
-                shutil.copyfile(default_analysis_path_s, analysis / 'saxs.ipynb')
-                shutil.copyfile(default_analysis_path_w, analysis / 'waxs.ipynb')
-                
-
-
-
-            if overwrite_dest and dest.exists():
-                dest.unlink()
-            dest.symlink_to(src)
-
-        except Exception:
-            tqdm.tqdm.write(f"FAILED: {dest}")
-            failed.append((uid, src, dest, analysis))
-        else:
-            tqdm.tqdm.write(f"Linked: {dest}")
-            linked.append((uid, src, dest, analysis))
-    return linked, failed
-
+def detector_mapping(detector):
+    if detector in {"pilatus300k-1", "pilatus800k-2"}:
+        subdir = "maxs/raw"
+        detname = "maxs"
+    elif detector == "pilatus2m-1":
+        subdir = "saxs/raw"
+        detname = "saxs"
+    elif detector == "pilatus800k-1":
+        subdir = "waxs/raw"
+        detname = "waxs"
+    else:
+        logger.error("WARNING: Can't do file handling for detector '{}'.".format(detector))
+        return None, None
+    
+    return subdir, detname
 
 @task
-def create_symlinks(ref, *, det_map=None, root_map=None):
+def create_symlinks(ref):
     """
     Parameters
     ----------
     ref : Union[int, str]
         Scan_id or uid of the start document
-    det_map : dict[str, str]
-        A dictionaly mapping the detector name (1M, 900KW)
-        to the type of measurement (SAXS, WAXS)
-    root_map : dict[str, str], optional
-        A mapping of root in the resource document -> a new path
-        as in databroker
 
     Returns
     -------
@@ -140,116 +40,25 @@ def create_symlinks(ref, *, det_map=None, root_map=None):
          A tuple of the start uid, the source path and the destination path.
     """
     logger = get_run_logger()
-    # ########################
-    # if root_map is None:
-    #     root_map = {}
 
-    # links = []
-    # target_template: str
-    # output_path: str
-    # resource_info = {}
-    # datum_info = {}
-    # target_keys = set()
-    # ########################
+    hrf = tiled_client_raw[ref]
+    for name, doc in hrf.documents():
+        if name == "start":
+            detectors = doc.get("detectors", [])
+            link_root = doc.get("experiment_alias_directory", None)
 
-    # hrf = tiled_client_raw[ref]
-    # for name, doc in hrf.documents():
-    #     if name == "start":
-    #         start_uid = doc["uid"]
-    #         #target_template = (f"{{det_name}}/{doc['username']}_{doc['sample_name']}_"
-    #         #                   f"id{doc['scan_id']}_{{N:06d}}_{{det_type}}.tif")
-    #         target_template = (f"{{det_name}}/{doc['sample_name']}_"
-    #                            f"id{doc['scan_id']}_{{N:06d}}_{{det_type}}.tif")
+        elif name == "resource":
+            for det in detectors:
+                if det in doc["root"]:
+                    subdir, detname = detector_mapping(det)
+                    prefix = str(Path(doc["root"]) / doc["resource_path"] / doc["resource_kwargs"]["filename"])
 
-    #         target_path = Path(
-    #             (f"/nsls2/data/smi/proposals/{doc['cycle']}/{doc['data_session']}/"
-    #             f"projects/{doc['project_name']}/user_data")
-    #         )
-    #         analysis_path = Path(
-    #             (f"/nsls2/data/smi/proposals/{doc['cycle']}/{doc['data_session']}/"
-    #             f"projects/{doc['project_name']}/analysis")
-    #         )
+                    for file_name in glob.glob(prefix + "*"):
+                        savename = os.path.splitext(os.path.basename(file_name))[0]  # only file name w/o extension
+                        link_name = Path(link_root) / subdir / f"{savename}_{detname}.tiff"
+                        os.symlink(file_name, link_name)
+                        logger.info(f"Linked: {file_name} to {link_name}")
 
-    #     elif name == "resource":
-
-    #         if doc["spec"] != "AD_TIFF":
-    #             continue
-    #         doc_root = doc["root"]
-    #         resource_info[doc["uid"]] = {
-    #             "path": Path(root_map.get(doc_root, doc_root)) / doc["resource_path"],  # noqa: 501
-    #             "kwargs": doc["resource_kwargs"],
-    #         }
-    #     elif "datum" in name:
-    #         if name == "datum":
-    #             doc = event_model.pack_datum_page(doc)
-
-    #         for datum_uid, point_number in zip(
-    #             doc["datum_id"], doc["datum_kwargs"]["point_number"]
-    #         ):
-    #             datum_info[datum_uid] = (
-    #                 resource_info[doc["resource"]],
-    #                 point_number,
-    #             )
-
-    #     elif name == "descriptor":
-    #         for k, v in doc["data_keys"].items():
-    #             if "external" in v:
-    #                 target_keys.add(k)
-    #     elif "event" in name:
-    #         # continue building the target_template here adding
-    #         # the event level things (motor positions)
-    #         if name == "event":
-    #             doc = event_model.pack_event_page(doc)
-    #         single_doc_data = {key:doc['data'][key][0] for key in doc['data']}
-    #         for key in target_keys:
-
-    #             det, _, _ = key.partition("_")
-    #             det_name = det.removeprefix("pil")
-    #             det_type = det_map.get(det_name, det_name)
-
-    #             if key not in doc["data"]:
-    #                 continue
-
-    #             for datum_id in doc["data"][key]:
-    #                 # pulling out the image column
-    #                 resource_vals, point_number = datum_info[datum_id]
-    #                 orig_template = resource_vals["kwargs"]["template"]
-    #                 fpp = resource_vals["kwargs"]["frame_per_point"]
-    #                 base_fname = resource_vals["kwargs"]["filename"]
-
-    #                 for fr in range(fpp):
-    #                     source_path = Path(
-    #                         orig_template
-    #                         % (
-    #                             str(resource_vals["path"]) + "/",
-    #                             base_fname,
-    #                             point_number * fpp + fr,
-    #                         )
-    #                     )
-
-                        
-    #                     dest_path = target_path / target_template.format(
-    #                         det_name=det_name,
-    #                         N=point_number * fpp + fr,
-    #                         det_type=det_type,
-    #                         **single_doc_data
-    #                     ).format(**single_doc_data)
-                        
-    #                     links.append(
-    #                         (start_uid, source_path, dest_path, analysis_path)
-    #                     )
-
-    #     elif name == "stop":
-    #         break
-
-    # linked, failed = do_symlinking(links, overwrite_dest=True)
-
-
-
-    # if len(linked) > 0:
-    #     logger.info(f"Links successfully generated: {linked}")
-    # if len(failed) > 0:
-    #     logger.info(f"Failed generating links: {failed}")
-
-
-    logger.info("TEST: Creating symlinks")
+                    break
+            else:
+                logger.error(f"Resource document referencing unknown detector {det}.")
